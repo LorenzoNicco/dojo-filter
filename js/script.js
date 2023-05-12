ff7a805e5d3f84ebd53368a6768702c1234d3ea9
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-05-12 17:24:40 +0200

    Add variabili per immagini

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let image = 'upload-file.png';
+
+let imgUrl = 'img/'+ image;
+
+const imgTag = document.getElementById('imgTag');
+
+imgTag.src = imgUrl;
+
+const inputButton = document.getElementById('myInputImg');
+
