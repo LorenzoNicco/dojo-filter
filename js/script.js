f5fd1c00b8e5f0a303f4dcd175686922c464974e
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-05-15 15:34:03 +0200

    Add filtri foto

--- js/script.js
+++ js/script.js
@@ -12,9 +12,59 @@ inputButton.onchange = () => {
 }
 
 // parte input----------------------------
+
 // grayscale
 const inputGrayscale = document.getElementById('grayscale');
 
 inputGrayscale.onchange = () => {
     imgTag.style.filter = "grayscale("+inputGrayscale.value+")";
+}
+
+// saturate
+const inputSaturate = document.getElementById('saturate');
+
+inputSaturate.onchange = () => {
+    imgTag.style.filter = "saturate("+inputSaturate.value+")";
+}
+
+// sepia
+const inputSepia = document.getElementById('sepia');
+
+inputSepia.onchange = () => {
+    imgTag.style.filter = "sepia("+inputSepia.value+")";
+}
+
+// invert
+const inputInvert = document.getElementById('invert');
+
+inputInvert.onchange = () => {
+    imgTag.style.filter = "invert("+inputInvert.value+")";
+}
+
+// contrast
+const inputContrast = document.getElementById('contrast');
+
+inputContrast.onchange = () => {
+    imgTag.style.filter = "contrast("+inputContrast.value+")";
+}
+
+// brightness
+const inputBrightness = document.getElementById('brightness');
+
+inputBrightness.onchange = () => {
+    imgTag.style.filter = "brightness("+inputBrightness.value+")";
+}
+
+// blur
+const inputBlur = document.getElementById('blur');
+
+inputBlur.onchange = () => {
+    imgTag.style.filter = "blur("+inputBlur.value+"px)";
+}
+
+// hue-rotate
+const inputHueRotate = document.getElementById('hue-rotate');
+
+inputHueRotate.onchange = () => {
+    imgTag.style.filter = "hue-rotate("+inputHueRotate.value+"deg)";
 }
